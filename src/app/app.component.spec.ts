8d605c8e5f8763f8e4e53a1194bcc838e5a7e25f
SSSagare <102542150+Sakshi0305@users.noreply.github.com>
2024-01-30 23:55:45 +0530

    Assignment11-2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'assign_11_1' title`, () => {
+  it(`should have the 'AngularAssignment11-2' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('assign_11_1');
+    expect(app.title).toEqual('AngularAssignment11-2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, assign_11_1');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularAssignment11-2');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { DemoComponent } from './demo/demo.component';
+import { Child1Component } from './child1/child1.component';
+import { Child2Component } from './child2/child2.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, DemoComponent],
+  imports: [CommonModule, RouterOutlet, Child1Component, Child2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'assign_11_1';
+  title = 'assign_11_2';
 }
